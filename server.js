c6e437070fee3ca43dd28c1457659fc0ca703b48
Akshayms02 <akshayms02@gmail.com>
2024-03-14 15:47:40 +0530

    Done

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const connectDB = require("./database/dataBase");
 const flash = require("express-flash");
 const methodOverride = require("method-override");
 const app = express();
+const createError = require("http-errors");
 
 const PORT = process.env.PORT || 3002;
 
@@ -43,6 +44,20 @@ app.use("/", nocache());
 
 app.use("/", userRoute);
 app.use("/admin", adminRoute);
+
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {}; 
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error',{ layout: false});
+});
 
 app.listen(PORT, () => {
   console.log(
