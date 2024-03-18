f1f50ad4126193a478347579eaae57cf8826a987
Akshayms02 <akshayms02@gmail.com>
2024-03-18 14:43:52 +0530

    Done

--- controllers/user/couponController.js
+++ controllers/user/couponController.js
@@ -2,14 +2,16 @@ const couponHelper = require("../../helper/couponHelper");
 
 const applyCoupon = async (req, res) => {
   try {
-
+    const price = parseInt(req.query.price);
     const userId = req.session.user;
     const couponCode = req.body.couponCode;
-
-    const result = await couponHelper.applyCoupon(userId, couponCode);
-    
-    console.log(result)
-    res.json(result);
+    if (price > 1500) {
+      const result = await couponHelper.applyCoupon(userId, couponCode);
+      console.log(result);
+      res.json({ result:result,status:true,message:"Coupon Applied Successfuly" });
+    } else {
+      res.json({ message: "Please purchase for 1500 above to apply coupon" ,status:false});
+    }
   } catch (error) {
     console.log(error);
   }
