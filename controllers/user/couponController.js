9b7617b483aae3a1cba1243852553b9206389e9d
Akshayms02 <akshayms02@gmail.com>
2024-03-20 11:32:56 +0530

    minor bugs in coupon fixed

--- controllers/user/couponController.js
+++ controllers/user/couponController.js
@@ -8,7 +8,12 @@ const applyCoupon = async (req, res) => {
     if (price > 1500) {
       const result = await couponHelper.applyCoupon(userId, couponCode);
       console.log(result);
-      res.json({ result:result,status:true,message:"Coupon Applied Successfuly" });
+      if (result.status) {
+        res.json({ result:result,status:true,message:"Coupon Applied Successfuly" }); 
+      } else {
+        res.json({ result:result,status:true,message:result.message }); 
+      }
+  
     } else {
       res.json({ message: "Please purchase for 1500 above to apply coupon" ,status:false});
     }
